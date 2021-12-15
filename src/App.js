7855225dbed2f7a9b9847d86045951311eff4fb5
Gokaran Jindal <kjindal0802@gmail.com>
2021-12-15 12:11:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import ReactToPrint from "react-to-print";
 
-function App() {
+const ComponentToPrint = React.forwardRef((props, ref) => (
+  <table ref={ref}>
+    <thead>
+      <th>column 1</th>
+      <th>column 2</th>
+      <th>column 3</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+      <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+      </tr>{" "}
+    </tbody>
+  </table>
+));
+
+const onClickHandler = (e) => {
+  e.preventDefault();
+  console.log("onClickHandler");
+};
+
+const App = React.forwardRef(() => {
+  const componentRef = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        style={{ marginLeft: "auto" }}
+        ref={componentRef}
+        onSubmit={onClickHandler}
+      >
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        Name : <input type="text" /> <br />
+        Email: <input type="email" /> <br />
+        Password: <input type="password" /> <br />
+        <ReactToPrint
+          trigger={() => <button type="submit">Print this out!</button>}
+          content={() => componentRef.current}
+        />
+      </form>
+
+      {/* <ComponentToPrint ref={componentRef} /> */}
     </div>
   );
-}
+});
 
 export default App;
